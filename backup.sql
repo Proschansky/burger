f08ccb3784294acd777d27c18df01cdeb8e5c55b
Brzeczyszczykiewicz <Samproschansky@gmail.com>
2019-03-05 18:14:55 -0500

    Fixed database connection.

--- backup.sql
+++ backup.sql
@@ -4,12 +4,12 @@ USE burger_db;
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	burgerName varchar(255) NOT NULL,
-	isEaten BOOLEAN NOT NULL DEFAULT 0,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN NOT NULL DEFAULT 0,
 	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
 
-INSERT INTO burgers (burgerName) VALUES ('Bacon Cheeseburger');
-INSERT INTO burgers (burgerName) VALUES ('Maui Waui Burger');
-INSERT INTO burgers (burgerName) VALUES ('Black and Blue Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Maui Waui Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black and Blue Burger');
