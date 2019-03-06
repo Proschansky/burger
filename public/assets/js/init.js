86b88909989871078d8657bebc4ad522f25222b4
Brzeczyszczykiewicz <Samproschansky@gmail.com>
2019-03-06 08:31:17 -0500

    Still working with the DOM.

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -1,7 +1,61 @@
-(function($){
-  $(function(){
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $("#notComplete").on("click", function() {
+    var id = $(this).data("id");
+    var devoured = $(this).data("devoured");
+    console.log(id, devoured)
 
-    $('.sidenav').sidenav();
+    var eaten = {
+      devoured: true
+    };
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+    // Send the PUT request.
+    $.ajax("/:id", {
+      type: "PUT",
+      data: eaten
+    }).then(
+      function() {
+        console.log("This burger has been devoured");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newCat = {
+      name: $("#ca").val().trim(),
+      sleepy: $("[name=sleepy]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/cats", {
+      type: "POST",
+      data: newCat
+    }).then(
+      function() {
+        console.log("created new cat");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".delete-cat").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/cats/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted cat", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
